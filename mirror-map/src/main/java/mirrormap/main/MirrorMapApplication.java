85884794f37922b9cc9472d3fc9b9afc672cca48
Juno <jvmeifert@gmail.com>
2024-04-01 19:55:58 -0400

    most of map backend workflow

--- mirror-map/src/main/java/mirrormap/main/MirrorMapApplication.java
+++ mirror-map/src/main/java/mirrormap/main/MirrorMapApplication.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import mirrormap.geoip.GeoIPDatabase;
+import mirrormap.geoip.GeoIPUpdater;
 import mirrormap.io.WebsocketFrame;
 import mirrormap.server.WebsocketController;
 import mirrormap.server.WebsocketServer;
@@ -16,27 +19,47 @@ public class MirrorMapApplication {
 
     public static void main(String[] args){
         try (ZContext context = new ZContext()) {
+            // Connect to metrics engine
             ZMQ.Socket socket = context.createSocket(SocketType.SUB);
             socket.connect("tcp://mirror-metrics:8081");
             socket.subscribe("");
 
+            // Start websocket server
             WebsocketServer websocketServer = new WebsocketServer(8080);
             websocketServer.start();
 
+            // Start GeoIP database updater
+            Thread geoIPUpdater = new Thread(new GeoIPUpdater());
+            geoIPUpdater.start();
+
+            // Get GeoIP database handle
+            GeoIPDatabase geoIP = GeoIPDatabase.getInstance();
+
+            // Get websocket controller handle
             WebsocketController websocketController = WebsocketController.getInstance();
 
+
             while(true) {
-                websocketController.broadcast(
-                        new WebsocketFrame((byte) 0x1, socket.recv())
-                );
+                String msg = new String(socket.recv(), StandardCharsets.UTF_8);
+                String[] projectIp = msg.split(":", 2);
+                if(projectIp.length == 2) {
+                    try {
+                        double[] latlong = geoIP.getLatLong(projectIp[1]);
+                        System.out.println(latlong[0] + " " + latlong[1]);
+                        websocketController.broadcast(
+                                new WebsocketFrame((byte) 0x1, new byte[]{'t'})
+                        );
+                    } catch (GeoIp2Exception ignored) {
+                        System.err.println("GeoIp2Exception");
+                    }
+                }
             }
 
 
 
 
             //DatabaseHandler maxmind = DatabaseHandler.getInstance();
-            //Thread maxmindUpdater = new Thread(new DatabaseUpdater());
-            //maxmindUpdater.start();
+
             
             //Thread.sleep(10000);
             //double [] latlong = maxmind.getLatLong("128.153.197.71");
