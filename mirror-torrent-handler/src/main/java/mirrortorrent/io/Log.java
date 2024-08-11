d2909a245f815bf4d1536701421c5637516cce69
carykees98 <trainboy980@gmail.com>
2024-08-11 11:55:38 -0400

    updated Log.java

--- mirror-torrent-handler/src/main/java/mirrortorrent/io/Log.java
+++ mirror-torrent-handler/src/main/java/mirrortorrent/io/Log.java
@@ -11,6 +11,7 @@ import java.util.Date;
  * Singleton class handling logging to the console and an external log server.
  */
 public class Log {
+
     private static final SimpleDateFormat LOG_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     private static final int COMPONENT_PAD = 24;
     private static final String LOG_DEBUG = " \u001B[34m[ DEBUG ]\u001B[0m ";
@@ -30,19 +31,25 @@ public class Log {
     /**
      * Possible levels of a log event.
      */
-    public enum Levels { INFO, WARN, ERROR, FATAL, DEBUG }
+    public enum Levels {
+        DEBUG, INFO, WARN, ERROR, FATAL
+    }
 
     /**
      * Gets the instance of Log.
+     *
      * @return Singleton instance of Log
      */
     public static synchronized Log getInstance() {
-        if(instance == null) { instance = new Log(); }
+        if (instance == null) {
+            instance = new Log();
+        }
         return instance;
     }
 
     /**
      * Configures and connects this instance of Log to the log server
+     *
      * @param log_host Log server hostname (ex. "mirrorlog" or "localhost")
      * @param log_port Log server port (ex. 4001)
      */
@@ -55,7 +62,7 @@ public class Log {
             clientOut = socket.getOutputStream();
             configured = true;
             printToConsole(Levels.INFO, "Logger - Connected to log server.");
-        } catch(IOException e) {
+        } catch (IOException e) {
             configured = false;
             printToConsole(Levels.ERROR, "Logger - Could not connect to log server.");
             printToConsole(Levels.WARN, "Logger - Running in offline mode.");
@@ -65,47 +72,65 @@ public class Log {
     /**
      * Constructs a Log.
      */
-    private Log() { configured = false; }
+    private Log() {
+        configured = false;
+    }
 
     /**
-     * Logs an event with severity "DEBUG".
-     * Debug log events are not sent to the log server.
+     * Logs an event with severity "DEBUG". Debug log events are not sent to the
+     * log server.
+     *
      * @param message Message to log
      */
-    public void debug(String message) { printToConsole(Levels.DEBUG, message); }
+    public void debug(String message) {
+        printToConsole(Levels.DEBUG, message);
+    }
 
     /**
      * Logs an event with severity "INFO".
+     *
      * @param message Message to log
      */
-    public void info(String message) { print(Levels.INFO, message); }
+    public void info(String message) {
+        print(Levels.INFO, message);
+    }
 
     /**
      * Logs an event with severity "WARN".
+     *
      * @param message Message to log
      */
-    public void warn(String message) { print(Levels.WARN, message); }
+    public void warn(String message) {
+        print(Levels.WARN, message);
+    }
 
     /**
      * Logs an event with severity "ERROR".
+     *
      * @param message Message to log
      */
-    public void error(String message) { print(Levels.ERROR, message); }
+    public void error(String message) {
+        print(Levels.ERROR, message);
+    }
 
     /**
      * Logs an event with severity "FATAL".
+     *
      * @param message Message to log
      */
-    public void fatal(String message) { print(Levels.FATAL, message); }
+    public void fatal(String message) {
+        print(Levels.FATAL, message);
+    }
 
     /**
      * Logs a message to the console and the log server.
+     *
      * @param level Severity of the log event
      * @param message Message to log
      */
     private synchronized void print(Levels level, String message) {
         printToConsole(level, message);
-        if(configured) {
+        if (configured) {
             StringBuilder sb = new StringBuilder();
             sb.append("@").append(component_name).append("@");
             sb.append(level.ordinal()).append(message).append("\n");
@@ -119,7 +144,7 @@ public class Log {
                     clientOut = socket.getOutputStream();
                     clientOut.write(sb.toString().getBytes(StandardCharsets.UTF_8));
                     clientOut.flush();
-                } catch(IOException f) {
+                } catch (IOException f) {
                     printToConsole(Levels.ERROR, "Logger - Could not reconnect to log server.");
                 }
             }
@@ -128,18 +153,24 @@ public class Log {
 
     /**
      * Prints a log event to the console.
+     *
      * @param level Severity of the log event
      * @param message Message to log
      */
     private void printToConsole(Levels level, String message) {
         StringBuilder sb = new StringBuilder();
         sb.append(LOG_DATE_FORMAT.format(new Date()));
-        switch(level.ordinal()) {
-            case 1 -> sb.append(LOG_WARN);
-            case 2 -> sb.append(LOG_ERROR);
-            case 3 -> sb.append(LOG_FATAL);
-            case 4 -> sb.append(LOG_DEBUG);
-            default -> sb.append(LOG_INFO);
+        switch (level) {
+            case DEBUG ->
+                sb.append(LOG_DEBUG);
+            case INFO ->
+                sb.append(LOG_INFO);
+            case WARN ->
+                sb.append(LOG_WARN);
+            case ERROR ->
+                sb.append(LOG_ERROR);
+            case FATAL ->
+                sb.append(LOG_FATAL);
         }
         sb.append(component_name);
         sb.append(" ".repeat(Math.max(0, COMPONENT_PAD - component_name.length())));
